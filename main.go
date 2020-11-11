df82b8c3993caef28098cefbc28ed878b4f7f78f
Abtin <aafshar@goodlabs.studio>
2020-11-10 21:48:36 -0500

    Second commit

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 // main.go
 package main
 
-func main() {
-  println("Ba dum, tss!")
-}
+import "fmt"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
+
+func main() {
+	fmt.Printf("my app %s, commit %s, built at %s by %s", version, commit, date, builtBy)
+}
